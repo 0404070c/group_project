edf591175da2f978390bef031ada6f5d4cf3bb73
Graeme Cairns <0404070c@student.gla.ac.uk>
2017-03-08 17:24:26 +0000

    fixed register page

--- rango/forms.py
+++ rango/forms.py
@@ -47,7 +47,9 @@ class PageForm(forms.ModelForm):
         #fields = ('title', 'url', 'views')
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+
 
     class Meta:
         model = User
