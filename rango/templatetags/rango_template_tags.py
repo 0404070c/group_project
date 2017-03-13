1323dfd2c2e92354bee35ddf69cbf423409ce08a
Gabriel Doucet <gabriel.doucet@eleve.enac.fr>
2017-03-13 19:19:37 +0000

    Working links in the templates

--- rango/templatetags/rango_template_tags.py
+++ rango/templatetags/rango_template_tags.py
@@ -8,4 +8,7 @@ def get_category_list(cat=None):
     return {'cats': Category.objects.all(),
         'act_cat': cat}
 
-
+@register.inclusion_tag('rango/album-list2.html')
+def get_album_list(album=None):
+    return {'albums': Category.objects.all(),
+        'act_alb': album}
