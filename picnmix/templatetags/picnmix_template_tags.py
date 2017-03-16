73f8cc8ff9d61ba5aac4ee73c5829fb84ae2393d
Gabriel Doucet <gabriel.doucet@eleve.enac.fr>
2017-03-16 12:47:14 +0000

    Adding cover photos

--- picnmix/templatetags/picnmix_template_tags.py
+++ picnmix/templatetags/picnmix_template_tags.py
@@ -10,16 +10,17 @@ def get_album_list(user=None):
   shared_albums = []
   for album in all_albums:
     shared_users = album.shared_users_as_list()
-    cover_photo = album.get_cover_photo(Photo.objects.all())
-    cover_photo_dict[album.album_name] = cover_photo
-    print cover_photo_dict
+
+    photos = Photo.objects.filter(album_id=album)
+    if len(photos) > 0:
+      album.cover_photo = photos[0].image
+      cover_photo_dict[album.album_name] = photos[0].image
+#    cover_photo_dict[album.album_name] = cover_photo
+    print album.cover_photo
 
     if not shared_users is None and (user.username in shared_users):
       shared_albums.append(album)
 
-    #    if photos:
-    #      album.coverPicture = photos[0].image
-    #      print album.coverPicture
   owned_albums = all_albums.filter(owner_id=user)
 
   return {'owned_albums': owned_albums, 'shared_albums': shared_albums, 'cover_photo_dict': cover_photo_dict}
