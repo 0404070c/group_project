c3675da98a34d8b027fa0fb3fa324f33c16d7426
Gabriel Doucet <gabriel.doucet@eleve.enac.fr>
2017-03-17 09:14:16 +0000

    Better album cover photo algorithm

--- picnmix/templatetags/picnmix_template_tags.py
+++ picnmix/templatetags/picnmix_template_tags.py
@@ -7,22 +7,22 @@ register = template.Library()
 def get_album_list(user=None):
   all_albums = Album.objects.all()
   shared_albums = []
+  owned_albums = []
 
   # Get the list of albums shared with the user and add a cover photo for every shared album
   for album in all_albums:
-    shared_users = album.shared_users_as_list()
-    if not shared_users is None and (user.username in shared_users):
-      photos = Photo.objects.filter(album_id=album)
-      if len(photos) > 0:
-        album.cover_photo = photos[0].image
-      shared_albums.append(album)
-
-  # Get the list of albums owned by the user
-  owned_albums = all_albums.filter(owner_id=user)
-  # Add a cover photo for every owned album
-  for album in owned_albums:
+    # Adding a cover photo for every album
     photos = Photo.objects.filter(album_id=album)
     if len(photos) > 0:
       album.cover_photo = photos[0].image
+
+    # Get shared user list from the album and populate the shared albums list
+    shared_users = album.shared_users_as_list()
+    if not shared_users is None and (user.username in shared_users):
+      shared_albums.append(album)
+    # Populate the owned albums list
+    if album.owner_id.username == user.username:
+      owned_albums.append(album)
+
 
   return {'owned_albums': owned_albums, 'shared_albums': shared_albums}
