9ebcf8c39c496e7f59e7a05557c1f3cfa73683e0
Graeme Cairns <0404070c@student.gla.ac.uk>
2017-03-21 15:59:00 +0000

    pythonanywhere fixes

--- picnmix_project/settings.py
+++ picnmix_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '2ooqwku2ze-+t=f(e68tw!ysu$o(z!e93kdmf)3vb=(xs&uin$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
